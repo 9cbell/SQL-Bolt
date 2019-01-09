378b8f00c86273454b76705eb6df0891b29d6cf8
9cbell <31480938+9cbell@users.noreply.github.com>
2019-01-09 14:04:25 -0600

    Add files via upload

--- exercises_1-5.sql
+++ exercises_1-5.sql
@@ -1,7 +1,12 @@
 --exercise 1
-
+SELECT title FROM movies;
+SELECT director FROM movies; 
+SELECT director, title FROM movies; 
+SELECT title, year FROM movies; 
+SELECT * FROM movies;
 --exercise 2
-
+SELECT id, title FROM movies
+WHERE id = 6;
 --exercise 3
 
 --exercise 4
