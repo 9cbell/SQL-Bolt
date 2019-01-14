ac68b846480e6d016236da6accc28dfa4051cf10
9cbell <31480938+9cbell@users.noreply.github.com>
2019-01-14 13:36:32 -0600

    Add files via upload

--- exercises_6-9.sql
+++ exercises_6-9.sql
@@ -1,10 +1,21 @@
 --exercise 6
 --6.1
 SELECT title, domestic_sales, international_sales 
-FROM movies
-  JOIN boxoffice
-    ON movies.id = boxoffice.movie_id;
-    
+  FROM movies
+    JOIN boxoffice
+      ON movies.id = boxoffice.movie_id;
+--6.2
+SELECT title, domestic_sales, international_sales
+  FROM movies
+    JOIN boxoffice
+      ON movies.id = boxoffice.movie_id
+  WHERE international_sales > domestic_sales;
+--6.3
+SELECT title, rating
+  FROM movies
+    JOIN boxoffice
+      ON movies.id = boxoffice.movie_id
+  ORDER BY rating DESC;
 --exercise 7
 
 --exercise 8
