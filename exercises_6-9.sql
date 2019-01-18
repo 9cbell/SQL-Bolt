103ed124413682dfe9cad4190c48fac9f4e49672
9cbell <31480938+9cbell@users.noreply.github.com>
2019-01-18 13:29:45 -0600

    Add files via upload

--- exercises_6-9.sql
+++ exercises_6-9.sql
@@ -48,8 +48,43 @@ SELECT title, rating * 10 AS rating_percent
     JOIN boxoffice
       ON movies.id = boxoffice.movie_id;--9.2
 --9.3
-SELECT title, year
+SELECT title, year 
   FROM movies
-  WHERE year % 2 = 0;
+  WHERE year % 2=0
 --exercise 10
 --10.1
+SELECT MAX(years_employed) as Max_years_employed
+  FROM employees;
+--10.2
+SELECT role, AVG(years_employed) as Average_years_employed
+  FROM employees
+  GROUP BY role;
+--10.3
+SELECT building, SUM(years_employed) as Total_years_employed
+  FROM employees
+  GROUP BY building;
+--exercise 11
+--11.1
+SELECT role, COUNT(*) as Number_of_artists
+  FROM employees
+  WHERE role = "Artist";
+--11.2
+SELECT role, COUNT(*)
+  FROM employees
+  GROUP BY role;
+--11.3
+SELECT role, SUM(years_employed)
+  FROM employees
+  GROUP BY role
+  HAVING role = "Engineer";
+--exercise 12
+--12.1
+SELECT director, COUNT(id) as Num_movies_directed
+  FROM movies
+  GROUP BY director;
+--12.2
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+  FROM movies
+      INNER JOIN boxoffice
+          ON movies.id = boxoffice.movie_id
+  GROUP BY director;
